30a409a078a63ec8f1ea649478d89f74e990597c
DmytroTHR <dtyagur@gmail.com>
2022-06-21 18:06:11 +0300

    some minor changes

--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"context"
+	"fmt"
 	"log"
 	"net/http"
 	"net/url"
@@ -15,7 +17,11 @@ import (
 )
 
 const (
-	defaultTimeout = 5 * time.Second
+	defaultTimeout   = 5 * time.Second
+	crawlingDepth    = 3
+	providedLink     = "https://4club.com.ua/"
+	wholeTestTimeout = time.Minute
+	resultFile       = "results.txt"
 )
 
 type Config struct {
@@ -33,10 +39,8 @@ type TestResult struct {
 }
 
 func main() {
-	if len(os.Args) < 2 {
-		log.Panic("You should provide a hostname")
-	}
-	providedLink := os.Args[1]
+	log.Printf("Checking path: %s", providedLink)
+
 	providedURL, err := url.Parse(providedLink)
 	if err != nil {
 		log.Panic(err)
@@ -45,13 +49,17 @@ func main() {
 	app := &Config{
 		URL: providedURL,
 	}
-	log.Printf("Hostname: %s", app.URL.Hostname())
-	
+
 	testResult := []TestResult{}
 
-	execResult := app.LinksCheck()
+	ctx, cancel := context.WithTimeout(context.Background(), wholeTestTimeout)
+	defer cancel()
+	execResult := app.LinksCheck(ctx)
 	execResult.Range(func(link, value interface{}) bool {
 		valueRes := value.(ExecutionResult)
+		if valueRes == (ExecutionResult{}) {
+			return true
+		}
 		testResult = append(testResult, TestResult{
 			ExecutionResult: valueRes,
 			Link:            link.(string),
@@ -62,39 +70,68 @@ func main() {
 	sort.Slice(testResult, func(i, j int) bool {
 		return testResult[i].StatusCode > testResult[j].StatusCode
 	})
-	for _, test := range testResult {
-		log.Printf("Err:%t, %d - %s :\t%s\n", test.IsError, test.StatusCode, test.Status, test.Link)
-	}
+
+	app.WriteResultToFile(testResult)
 }
 
-func (app *Config) LinksCheck() *sync.Map {
+func (app *Config) WriteResultToFile(testResult []TestResult) {
+	file, err := os.Create(resultFile)
+	if err != nil {
+		log.Panic(err)
+	}
+	defer file.Close()
+
+	for _, test := range testResult {
+		file.WriteString(fmt.Sprintf("Err:%t, %d - %s :\t%s\n", test.IsError, test.StatusCode, test.Status, test.Link))
+	}
+	log.Println("Result saved to file: ", resultFile)
+}
+
+func (app *Config) LinksCheck(ctx context.Context) *sync.Map {
 	res := sync.Map{}
+	hostname := app.URL.Hostname()
 
 	col := colly.NewCollector(
-		colly.AllowedDomains(app.URL.Hostname()),
-		colly.MaxDepth(3),
-		colly.MaxBodySize(0),
+		colly.AllowedDomains(hostname),
+		colly.MaxDepth(crawlingDepth),
+		colly.MaxBodySize(1024*1024),
 		colly.IgnoreRobotsTxt(),
 		colly.Async(),
+		colly.ParseHTTPErrorResponse(),
+		colly.UserAgent("Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:101.0) Gecko/20100101 Firefox/101.0"),
 	)
 
-	col.Limit(&colly.LimitRule{Parallelism: runtime.NumCPU()})
+	col.Limit(&colly.LimitRule{
+		Parallelism:  runtime.NumCPU() / 2,
+		RandomDelay:  defaultTimeout / 100,
+		DomainRegexp: fmt.Sprintf(".*%s.*", strings.ReplaceAll(hostname, ".", "\\.")),
+	})
 	col.SetRequestTimeout(defaultTimeout)
 
-	col.OnHTML(`[href]`, collyFunc(&res, "href", app.URL.Hostname()))
-	col.OnHTML(`[src]`, collyFunc(&res, "href", app.URL.Hostname()))
-
-	col.OnResponse(func(r *colly.Response) {
-		res.Store(r.Request.URL.String(), ExecutionResult{
-			StatusCode: r.StatusCode,
-			Status:     http.StatusText(r.StatusCode),
-		})
+	col.OnRequest(func(request *colly.Request) {
+		select {
+		case <-ctx.Done():
+			request.Abort()
+		default:
+		}
 	})
-	col.OnError(func(r *colly.Response, err error) {
+	col.OnResponseHeaders(func(response *colly.Response) {
+		request := response.Request
+		select {
+		case <-ctx.Done():
+			request.Abort()
+		default:
+		}
+	})
+
+	col.OnHTML(`[href]`, collyFunc(&res, ctx, "href"))
+	col.OnHTML(`[src]`, collyFunc(&res, ctx, "src"))
+
+	col.OnScraped(func(r *colly.Response) {
 		res.Store(r.Request.URL.String(), ExecutionResult{
 			StatusCode: r.StatusCode,
 			Status:     http.StatusText(r.StatusCode),
-			IsError:    true,
+			IsError:    r.StatusCode >= 203,
 		})
 	})
 
@@ -108,10 +145,10 @@ func (app *Config) LinksCheck() *sync.Map {
 	return &res
 }
 
-func collyFunc(res *sync.Map, attrName, hostname string) func(e *colly.HTMLElement) {
+func collyFunc(res *sync.Map, ctx context.Context, attrName string) func(e *colly.HTMLElement) {
 	return func(e *colly.HTMLElement) {
 		link := e.Request.AbsoluteURL(e.Attr(attrName))
-		if link != "" && strings.Contains(link, hostname) {
+		if link != "" {
 			if _, ok := res.Load(link); !ok {
 				res.Store(link, ExecutionResult{})
 				e.Request.Visit(link)
